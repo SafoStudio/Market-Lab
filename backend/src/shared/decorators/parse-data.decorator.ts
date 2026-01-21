49539abdc1d923bc585a57bd1c0a7f026ec7408f
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2026-01-21 12:53:53 +0200

    Update parse-data.decorator.ts

--- backend/src/shared/decorators/parse-data.decorator.ts
+++ backend/src/shared/decorators/parse-data.decorator.ts
@@ -1,22 +1,24 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const ParseData = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (dtoClass: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const body = request.body;
 
-    if (body && typeof body === 'object') {
-      for (const key in body) {
-        if (typeof body[key] === 'string') {
-          try {
-            body[key] = JSON.parse(body[key]);
-          } catch {
-            // Not JSON - leave it as is
-          }
+    if (!body || typeof body !== 'object') return body;
+
+    for (const key in body) {
+      if (typeof body[key] === 'string') {
+        try {
+          body[key] = JSON.parse(body[key]);
+        } catch {
+          // Not JSON - leave it as is
         }
       }
     }
 
-    return body;
+    const data = body.data || body;
+
+    return data;
   },
 );
